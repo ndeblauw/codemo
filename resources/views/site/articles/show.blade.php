270d9d22c18a8636357e98f752145cf12340026e
Nico Deblauwe <nico@bluepundit.eu>
2025-02-26 23:40:46 +0700

    FEATURE :sparkles: Add author info to article

--- resources/views/site/articles/show.blade.php
+++ resources/views/site/articles/show.blade.php
@@ -1,5 +1,6 @@
 <x-site-layout>
     <h1 class="text-4xl font-bold">{{$article->title}}</h1>
+    <h2 class="text-xl mt-2">Written by <span class="font-semibold">{{$article->author->name}}</span></h2>
 
     <p class="mt-4">
         {{$article->content}}

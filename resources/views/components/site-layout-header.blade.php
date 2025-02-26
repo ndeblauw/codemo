e17f9cb731161281aafee1134e1b8f9f3884b459
Nico Deblauwe <nico@bluepundit.eu>
2025-02-26 23:19:20 +0700

    DESIGN :art: Make the navbar configurable and beautiful

--- resources/views/components/site-layout-header.blade.php
+++ resources/views/components/site-layout-header.blade.php
@@ -1,10 +1,11 @@
 <header class="bg-purple-600 text-purple-50 py-4">
     <div class="max-w-6xl mx-auto flex justify-between items-center">
         <div class="font-bold text-white text-xl uppercase">Codemo</div>
-        <div>
-            <a href="/articles">Articles</a>
-            <a href="/authors">Authors</a>
-        </div>
+        <ul class="flex gap-x-8">
+            @foreach($menu as $label => $url)
+                <li class="hover:font-bold"><a href="{{$url}}">{{$label}}</a></li>
+            @endforeach
+        </ul>
         <div>login</div>
     </div>
 </header>

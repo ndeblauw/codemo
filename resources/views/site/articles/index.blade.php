113a1e4227340f28979a06958501f936893c389a
Nico Deblauwe <nico@bluepundit.eu>
2025-02-26 23:52:36 +0700

    DESIGN :art: Build a beautiful layout for the articles

--- resources/views/site/articles/index.blade.php
+++ resources/views/site/articles/index.blade.php
@@ -1,9 +1,14 @@
 <x-site-layout>
     <h1 class="font-bold text-4xl">Overview of the articles</h1>
 
-    <ul class="list-disc pl-4">
+    <ul class="grid grid-cols-3 gap-12 mt-12 ">
         @foreach($articles as $article)
-            <li><a class="underline" href="/articles/{{$article->id}}">{{ $article->title }}</a></li>
+            <li class="hover:bg-purple-50 p-2 border-t-2 border-black">
+                <a class="" href="/articles/{{$article->id}}">
+                    <h2 class="text-2xl font-bold">{{ $article->title }}</h2>
+                    <p class="line-clamp-3">{{$article->content}}</p>
+                </a>
+            </li>
         @endforeach
     </ul>
 </x-site-layout>

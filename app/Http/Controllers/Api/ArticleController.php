5600de7d23e7c4a584d6bf63f67b8692aca1011d
Nico Deblauwe <nico@bluepundit.eu>
2025-03-26 20:48:13 +0100

    FEATURE :sparkles: Add Create/Update/Destroy for articles

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -22,4 +22,54 @@ class ArticleController extends Controller
         return new ArticleShowResource($article);
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', 'string', 'min:10', 'max:255'],
+            'content' => ['required'],
+        ]);
+
+        $article = Article::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'author_id' => 1, // make dynamic again TODO
+            'is_public' => 1,
+        ]);
+
+        return response()->json([
+            'message' => 'Article created successfully',
+        ], 201);
+
+    }
+
+    public function update(Request $request, Article $article)
+    {
+        $request->validate([
+            'title' => ['required', 'string', 'min:10', 'max:255'],
+            'content' => ['required'],
+        ]);
+
+        // Business logic check based on logged in user retrieved from the token
+
+        $article->update([
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        return response()->json([
+            'message' => 'Article updated successfully',
+        ], 200);
+    }
+
+    public function destroy(Article $article)
+    {
+        // Business logic check based on logged in user retrieved from the token
+
+        $article->delete();
+
+        return response()->json([
+            'message' => 'Article deleted successfully',
+        ], 200);
+    }
+
 }

22ba20a7b7ede7f9077224537ef3f4ed338ba00b
Nico Deblauwe <nico@bluepundit.eu>
2025-04-04 10:14:10 +0200

    FEATURE :sparkles: Improve caching method

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,7 +8,7 @@ class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $latest_articles = cache()->remember('latest_articles', 10, function () {
+        $latest_articles = cache()->flexible('latest_articles', [5, 10], function () {
             return \App\Models\Article::latest()->take(5)->get();
         });
 

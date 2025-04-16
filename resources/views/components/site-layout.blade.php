ec69a90e704369c33ba79b59cef1628f8b26c04f
Nico Deblauwe <nico@bluepundit.eu>
2025-04-16 18:33:08 +0200

    FEATURE :sparkles: Using Vite for local semi-static asset management

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- uncomment the line below to use Vite, and comment the cdn import -->
+    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 <body>
